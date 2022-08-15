5c36454fa3f0c3659cbd3afbc15cedd824f549ce
AshkanArabim <ashkan.arabim@gmail.com>
2022-08-15 11:59:48 -0600

    add devtool

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World!"),console.log("just a test");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hello World!");
+console.log("just a lol");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EsMEIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhlbGxvIFdvcmxkIVwiKTtcbmNvbnNvbGUubG9nKFwianVzdCBhIGxvbFwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
